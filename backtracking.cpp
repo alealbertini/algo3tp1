d40cd827a98b502cf68a07f0f172be6d3bed6184
ale <alealbertini1@gmail.com>
2019-08-28 00:18:00 -0300

    terminado fase 1 backtracking

--- backtracking.cpp
+++ backtracking.cpp
@@ -3,72 +3,68 @@
 using namespace std;
 
 
+pair<int, int> backtracking(int *arrayPesos, int *arrayBeneficios, int ij, int n, int w, pair<int, int> solucionParcial){
+    
+    //for(int l = ij; l < n; l++){
+        //cout << "backtracking recursivo " << l << endl;
+
+        if(ij == n - 1){   // ES EL ULTIMO NODO POR LO TANTO ES UNA HOJA 
+
+          if(arrayPesos[ij] + solucionParcial.first <= w){
+              solucionParcial.first += arrayPesos[ij];
+              solucionParcial.second += arrayBeneficios[ij];
+              cout << "fin " << solucionParcial.second << endl;
+              return solucionParcial;
+          }
+        } else {
+
+          if(arrayPesos[ij] + solucionParcial.first <= w){
+              solucionParcial.first += arrayPesos[ij];
+              solucionParcial.second += arrayBeneficios[ij];
+              cout << "back " << solucionParcial.second << endl;
+              backtracking(arrayPesos, arrayBeneficios, ij + 1, n, w, solucionParcial);
+          } else {    // PODA: SI ESTE NODO NO ENTRA NO SIRVE SEGUIR PROBANDO CON ESTA COMBINACION
+                //cout << "break " << endl; 
+                //break;
+              backtracking(arrayPesos, arrayBeneficios, ij + 1, n, w, solucionParcial);
+          }
+            //for(int i = ij + 1; i < n; i++){
+                
+            //}
+        }
+    //}
+}
+
+
 int main ()
 {
   int n, w;
-  cin >> n >> w;
- 
-  int *pesos = new int[n];
+  //cin >> n >> w;
+  n = 5; w = 25;
+  /*int *pesos = new int[n];
   int *beneficios = new int[n];
 
   for(int i = 0; i < n; i++){
     cin >> pesos[i] >> beneficios[i];
-  }
+  }*/
 
-  int pesoAcumulado = 0;
+  int pesos [5] = { 10, 15, 5, 10, 5 };
+  int beneficios [5] = { 5, 4, 13, 8, 8 };
+
   int beneficioMaximo = 0;
-  int beneficioActual = 0;
-  int paquetesEnLaMochila = 0;
+  pair<int, int> solucion = make_pair(0, 0);  
 
-  for(int l = 0; l < n; l++){
+  for(int i = 0; i < n; i++){
 
-    pesoAcumulado = 0;
-    beneficioActual = 0;
-    paquetesEnLaMochila = 0;
+      cout << "backtracking " << i << endl;
 
-    for(int cantElementos = 1; cantElementos <= n; cantElementos++){
+      pair<int, int> sol = backtracking(pesos, beneficios, i, n, w, solucion);
 
-      pesoAcumulado = 0;
-      beneficioActual = 0;
-      paquetesEnLaMochila = 0;
-
-      for(int i = l; i < n; i++){
-
-        // PODA: NO AGREGAR EL PAQUETE SI YA SE QUE SE PASA DE LA CAPACIDAD DE LA MOCHILA. 
-        //        ADEMAS ME EVITO PROBAR TODAS LAS COMBINACIONES DE ESE PAQUETE.
-        if(pesoAcumulado + pesos[i] < w){   
-
-          paquetesEnLaMochila++;
-          pesoAcumulado += pesos[i];
-          beneficioActual += beneficios[i];
-
-          for(int j = i + 1; j < n; j++){
-             
-             if(paquetesEnLaMochila >= cantElementos){   
-                  break;
-             } else {
-                pesoAcumulado += pesos[j];
-
-                if(pesoAcumulado <= w){  // SI EL PAQUETE ENTRA EN LA MOCHILA, LO AGREGO
-
-                    beneficioActual += beneficios[j];
-
-                    if(beneficioActual > beneficioMaximo){
-                       beneficioMaximo = beneficioActual;
-                    }
-                }
-                // SI EL PAQUETE NO ENTRA EN LA MOCHILA, NO LO AGREGO Y ME FIJO SI ENTRA EL SIGUIENTE PAQUETE
-             }
-
-             pesoAcumulado -= pesos[j];
-             beneficioActual -= beneficios[j];
-             //cout << paquetesEnLaMochila << " " << cantElementos << endl;
-             //cout << "El beneficio de " << i << " " << j << " es: " << beneficioActual << endl;
-          }
-        } 
+      if(sol.second > beneficioMaximo){
+          beneficioMaximo = sol.second;
+          cout << "beneficioMaximo ahora es: " << beneficioMaximo << endl;
       }
-    }  
-  }
+  } 
 
   cout << beneficioMaximo << endl;
   return beneficioMaximo;
